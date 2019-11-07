df5126ec96874a769244c8cbdde3415b26115c42
stupidJoon <junsang.yu3@gmail.com>
2019-11-07 11:41:06 +0900

    a

--- socket.js
+++ socket.js
@@ -4,6 +4,9 @@ let nsp = { };
 
 module.exports.on = (io) => {
   io.on('connection', (socket) => {
-    console.log('Socket Connected');
+    console.log('Socket Connected', socket.id);
+    socket.on('disconnected', () => {
+      console.log('Socket Disconnected', socket.id);
+    });
   });
 }
