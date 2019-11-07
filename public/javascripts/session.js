fc77fd4d8f7253fc9d5d6996a5fa26105f08f51d
stupidJoon <junsang.yu3@gmail.com>
2019-11-08 01:04:15 +0900

    a

--- public/javascripts/session.js
+++ public/javascripts/session.js
@@ -1,7 +1,33 @@
+const RTC_CONFIGURATION = {
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+    {
+      url: 'turn:numb.viagenie.ca',
+      credential: 'muazkh',
+      username: 'webrtc@live.com'
+    }
+  ]
+};
 const SESSION_ID = location.href.split('/')[4];
-// const socket = io.connect('https://sunrintv.kro.kr');
 const socket = io.connect('');
+
+let sessionType;
+
+socket.on('session_type', (sessionType) => {
+  this.sessionType = sessionType;
+  if (sessionType == 'caller') {
+    $("#session_init").modal({ backdrop: 'static', keyboard: false });
+  }
+  console.log(sessionType);
+});
+
+socket.emit('join_session', SESSION_ID);
 
 $(document).ready(() => {
   console.log(SESSION_ID);
+  $("#sessionInputScreenSelectButton").click(() => {
+    navigator.mediaDevices.getDisplayMedia({ audio: false, video: true }).then((mediaStream) => {
+      
+    })
+  });
 });
--- socket.js
+++ socket.js
@@ -1,12 +1,38 @@
 var exports = module.exports = {};
 
-let nsp = { };
+let sessions = {};
 
 module.exports.on = (io) => {
   io.on('connection', (socket) => {
     console.log('Socket Connected', socket.id);
-    socket.on('disconnected', () => {
+    
+    socket.on('join_session', (session_id) => {
+      if (sessions[session_id] == undefined) {
+        sessions[session_id] = { 'caller': socket, 'callee': [] };
+        socket.emit('session_type', 'caller');
+      }
+      else {
+        sessions[session_id]['callee'].push(socket);
+        socket.emit('session_type', 'callee');
+      }
+      console.log('\nSession:', session_id, '\nCaller:', sessions[session_id]['caller'].id, 'Callee:', sessions[session_id]['callee'].length);
+    });
+
+    socket.on('disconnect', () => {
+      Object.keys(sessions).forEach((value1, index) => {
+        if (sessions[value1]['caller'] == socket) {
+          sessions[value1] = undefined;
+        }
+        else {
+          sessions[value1]['callee'].forEach((value2, index) => {
+            if (value2 == socket) {
+              sessions[value1]['callee'].splice(index);
+            }
+          });
+        }
+      });
       console.log('Socket Disconnected', socket.id);
     });
+
   });
 }
