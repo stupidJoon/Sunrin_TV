2c144b8cd529857631c9fd21b518a12fce97666d
stupidJoon <junsang.yu3@gmail.com>
2019-11-07 11:42:22 +0900

    a

--- public/javascripts/session.js
+++ public/javascripts/session.js
@@ -1,6 +1,6 @@
 const SESSION_ID = location.href.split('/')[4];
-const socket = io.connect('https://sunrintv.kro.kr');
-// const socket = io.connect('');
+// const socket = io.connect('https://sunrintv.kro.kr');
+const socket = io.connect('');
 
 $(document).ready(() => {
   console.log(SESSION_ID);
