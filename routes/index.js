b24434512a84d974f0e7bd4129506d1029eac9c6
stupidJoon <junsang.yu3@gmail.com>
2019-12-19 12:58:20 +0900

    a

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ router.get('/session', (req, res) => {
   res.redirect('/session/*' + shortid.generate());
 });
 router.get(/\/session\//, (req, res) => {
-  res.render(path.join(__dirname, '../views/session.jade'), { username: req.user.id });
+  res.render(path.join(__dirname, '../views/session.jade'), { user: req.user });
   // res.sendFile(path.join(__dirname, '../views/session.html'));
 });
 
